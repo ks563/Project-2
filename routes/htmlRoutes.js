75f3fe373f50022da073a30ad0604dc639c7b47e
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-03-27 19:43:16 -0500

    changed routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,27 @@
 var db = require("../models");
 var authorizeUser = require("../config/authorizeUser");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
   app.get("/home", authorizeUser, function(req, res) {
-    res.render("eventCreation")
+    res.sendFile(path.join(__dirname, "../public/html/eventCreation.html"));
+
     console.log(req.user)
   });
   app.get("/login", function(req, res) {
-    res.render("login")
+    res.sendFile(path.join(__dirname, "../public/html/login.html"));
     
   });
   app.get("/register", function(req, res) {
-    res.render("register")
+    res.sendFile(path.join(__dirname, "../public/html/register.html"));
   });
 
   app.get("/event/:id",authorizeUser, function(req, res) {
 
   });
   app.get("/event/create/:id",authorizeUser, function(req, res) {
-    res.render("addItems")
+    res.sendFile(path.join(__dirname, "../public/html/additems.html"));
   });
 
   // Render 404 page for any unmatched routes
