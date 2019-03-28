ad4fba0930ec93010c9522f13e8e1a7ac70ceba6
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-03-27 20:51:59 -0500

    cleaned up folders

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,18 @@
 var db = require("../models");
 var authorizeUser = require("../config/authorizeUser");
+var path = require("path")
 
 module.exports = function(app) {
   // Load index page
   app.get("/home", authorizeUser, function(req, res) {
-    res.render("eventCreation")
-    console.log(req.user)
+  
+  
+      res.sendFile(path.join(__dirname, "../views/eventCreation.html"));
   });
-  app.get("/login", function(req, res) {
-    res.render("login")
-    
-  });
-  app.get("/register", function(req, res) {
-    res.render("register")
+    app.get("/login", function(req, res) {
+      console.log(req.user)
+      res.sendFile(path.join(__dirname, "../views/login.html"));
+  
   });
 
   app.get("/event/:id",authorizeUser, function(req, res) {
